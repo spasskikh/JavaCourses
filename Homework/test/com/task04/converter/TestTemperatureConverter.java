c471697a2d1e1328fa2deee8981eb1c6173b7a5f
Anton <spasskikh@gmail.com>
2018-10-23 18:09:41 +0300

    task04 - converter - initial commit

---
+++ Homework/test/com/task04/converter/TestTemperatureConverter.java
@@ -0,0 +1,56 @@
+package com.task04.converter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTemperatureConverter {
+
+    private TemperatureConverter converter;
+
+    @BeforeEach
+    public void init() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    public void testConvertFtoC() {
+        assertEquals(38, Math.round(converter.convertFtoC(100)));
+    }
+
+    @Test
+    public void testConvertCtoF() {
+        assertEquals(212, converter.convertCtoF(100));
+    }
+
+    @Test
+    public void testConvertCtoK() {
+        assertEquals(303.2, converter.convertCtoK(30));
+    }
+
+    @DisplayName("Should convert Kelvin to Celsius")
+    @ParameterizedTest(name = "{index} => kelvin={0}, celsius={1}")
+    @CsvSource({
+            "0, -273.2",
+            "273.2, 0"
+    })
+    public void testConvertKtoC(double kelvin, double celsius) {
+        assertEquals(celsius, converter.convertKtoC(kelvin));
+    }
+
+    @Test
+    public void testConvertFtoK() {
+        assertNotEquals(10, converter.convertFtoK(100));
+    }
+
+    @Test
+    public void testConvertKtoF() {
+        assertNotEquals(10, converter.convertKtoF(0));
+    }
+
+
+}
